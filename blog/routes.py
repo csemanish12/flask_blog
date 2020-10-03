5d42616ff1b4a929210a112525be81aed67a16cd
Manish sah <sahmanish20@gmail.com>
2020-10-03 08:54:00 +0545

    add logic for handling profile update

--- blog/routes.py
+++ blog/routes.py
@@ -68,8 +68,31 @@ def logout():
     return redirect("/")
 
 
-@app.route("/profile")
+@app.route("/profile", methods=["GET", "POST"])
 @login_required
 def profile():
     form = ProfileUpdateForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        if form.picture.data:
+            # extract filename and extension from image uploaded by user
+            filename, file_extension = os.path.splitext(form.picture.data.filename)
+
+            # create new name for the image uploaded by user, as the multiple user can upload the image with same name
+            new_file_name = current_user.username + file_extension
+
+            # generate path for the saving the image
+            file_path = os.path.join(app.root_path, 'static/profile_pictures', new_file_name)
+
+            # save the image in that locoation
+            form.picture.data.save(file_path)
+
+            # save the name of file in our database
+            current_user.image_file = new_file_name
+        db.session.commit()
+        flash('Your profile was updated', 'success')
+        return redirect("/profile")
+    form.username.data = current_user.username
+    form.email.data = current_user.email
     return render_template('profile.html', title_name='profile', form=form)
